e3bc1d8222f34f7eb67a515446591993a66d992e
Bhalla, Kapil <Kapil_Bhalla@intuit.com>
2017-04-04 18:58:45 -0700

    **This checkin contains the following:** 1. At par of the earlier one

---
+++ whatsAtMovies/whatsAtMovies/MovieDetailsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MovieDetailsViewController.swift
+//  whatsAtMovies
+//
+//  Created by Bhalla, Kapil on 4/4/17.
+//  Copyright © 2017 Bhalla, Kapil. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    var url: String?
+    var movieDesc: String?
+
+    @IBOutlet weak var movieImage: UIView!
+    @IBOutlet weak var movieDetailsLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieDetailsContainer: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        movieImageView.setImageWith((URL(string: url!)!))
+        movieDetailsLabel.text = movieDesc
+        movieDetailsLabel.sizeToFit()
+        movieImage.bringSubview(toFront: movieDetailsContainer)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- whatsAtMovies/whatsAtMovies/moviesViewController.swift
+++ whatsAtMovies/whatsAtMovies/moviesViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import AFNetworking
+import MBProgressHUD
 
 class moviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     // reference to the table view showing the movies
     @IBOutlet weak var mTableView: UITableView!
+    
+    var posts: [NSDictionary]?
+    var selectedCell: PhotoCell?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +24,17 @@ class moviesViewController: UIViewController, UITableViewDataSource, UITableView
         mTableView.dataSource = self
         mTableView.delegate = self
         // Do any additional setup after loading the view.
+        
+        // Display HUD right before the request is made
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        moviesViewController.fetchMovies(successCallBack: setTableData, errorCallBack: handleErrors)
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(self.refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
+        
+        mTableView.insertSubview(refreshControl, at: 0)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,19 +42,98 @@ class moviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let cell = sender as! PhotoCell
+        
+        let destination = segue.destination as! MovieDetailsViewController
+        
+        destination.url = cell.mainImageURL
+        destination.movieDesc = cell.movieDesc
+    }
+    
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        // Display HUD right before the request is made
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        moviesViewController.fetchMovies(successCallBack: setTableData, errorCallBack: handleErrors)
+        
+        // Tell the refreshControl to stop spinning
+        refreshControl.endRefreshing()
+    }
+
     // Data source
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20;    }
+        if let posts = self.posts {
+            return posts.count
+        }
+        return 0
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
-        cell.textLabel!.text = "row \(indexPath.row)"
-        print("row \(indexPath.row)")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as! PhotoCell
+        
+        if let posts = self.posts {
+            cell.movieTitle.text = posts[indexPath.row]["title"] as! String
+            
+            let movieDetails = posts[indexPath.row]["overview"] as! String
+            cell.movieDetails.text = movieDetails
+            let posterPath = posts[indexPath.row]["poster_path"] as! String
+            let posterSize = "/w92"
+            let fullSize = "/original"
+            let posterImgUrl = "https://image.tmdb.org/t/p" + posterSize + posterPath
+            let fullImageUrl = "https://image.tmdb.org/t/p" + fullSize + posterPath
+            
+            cell.mainImageURL = fullImageUrl
+            cell.movieDesc = movieDetails
+            cell.posterImg.setImageWith(URL(string: posterImgUrl)!)
+        }
+
         return cell
     }
     
+    class func fetchMovies(successCallBack: @escaping (NSDictionary) -> (), errorCallBack: ((Error?) -> ())?) {
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 5)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if let error = error {
+                errorCallBack?(error)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                //print(dataDictionary)
+                successCallBack(dataDictionary)
+            }
+        }
+        task.resume()
+    }
     
+    func setTableData (movies: NSDictionary) {
+        // Hide HUD once the network request comes back (must be done on main UI thread)
+        MBProgressHUD.hide(for: self.view, animated: true)
+        
+        self.posts = movies["results"] as! [NSDictionary]
+        self.mTableView.reloadData()
+    }
+    
+    func handleErrors (error: Error?){
+        // Hide HUD once the network request comes back (must be done on main UI thread)
+        MBProgressHUD.hide(for: self.view, animated: true)
+        
+        let errorMessage = "check network connectivity"
+        let alertController = UIAlertController(title: "Network Error", message: errorMessage, preferredStyle: .alert)
+        alertController.show(self, sender: nil)
+    }
 
     /*
     // MARK: - Navigation
@@ -50,4 +145,15 @@ class moviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     */
 
+}
+
+
+class PhotoCell: UITableViewCell {
+    
+    @IBOutlet weak var posterImg: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDetails: UILabel!
+    
+    var mainImageURL: String?
+    var movieDesc: String?
 }
